33998f0023cd5f024e35ce94cc57c1636adb499c
Manabu Sakai <rea.527@gmail.com>
2024-09-23 09:18:08 +0900

    refactor: Remove unused chunk func

--- util.go
+++ util.go
@@ -16,17 +16,6 @@ func (d debugger) Printf(format string, args ...interface{}) {
 	}
 }
 
-func chunk(items []string, chunkSize int) (chunks [][]string) {
-	if len(items) == 0 {
-		return [][]string{}
-	}
-	for chunkSize < len(items) {
-		items, chunks = items[chunkSize:], append(chunks, items[:chunkSize])
-	}
-
-	return append(chunks, items)
-}
-
 func sleep() {
 	// Refill rate of API actions per second.
 	// https://docs.aws.amazon.com/AmazonECS/latest/APIReference/request-throttling.html
